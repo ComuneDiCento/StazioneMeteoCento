d7e2263e830d2e1499f443909fc05e20caf8239a
Matteo Fortini <matteo.fortini@gmail.com>
2025-07-09 19:13:13 +0200

    fix: sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import HeaderSticky from "bootstrap-italia/dist/plugins/header-sticky";
 import { CompleteHeader } from "./components";
 import WeatherDashboard from './WeatherDashboard';
 import Footer from "./components/Footer";
 
 import "./App.css";
-import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 
 export const App: FC = () => {
-    return (
+  useEffect(() => {
+    const el = document.querySelector<HTMLDivElement>(
+      ".it-header-wrapper.it-header-sticky"
+    );
+    if (el) {
+      HeaderSticky.getOrCreateInstance(el);
+    }
+  }, []);
+
+  return (
     <div className="App">
       <CompleteHeader />
       <div className="container my-4">
-      <WeatherDashboard />
-      <Footer />
+        <WeatherDashboard />
       </div>
+      <Footer />
     </div>
   );
 }
